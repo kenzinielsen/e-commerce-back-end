13d7dfbbc63fa0374e7499b25071949dc69b0530
kenzinielsen <kenzinielsen@kenzis-air.lan>
2021-10-23 22:20:15 -0600

    add product and category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,25 +4,41 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  Category.findAll({
+
+  })
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  Category.findOne({
+
+  })
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then()
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+
+  })
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+
+  })
 });
 
 module.exports = router;
